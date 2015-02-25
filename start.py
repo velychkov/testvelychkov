b33ba241ae098adc70024bc373c2cbd2a59a441d
Alex velychkov <Skyiner@gmail.com>
2015-02-25 20:51:02 +0200

    start 1 the first commit

---
+++ start.py
@@ -0,0 +1,11 @@
+Python 2.7.9 (default, Dec 10 2014, 12:24:55) [MSC v.1500 32 bit (Intel)] on win32
+Type "copyright", "credits" or "license()" for more information.
+>>> from bs4 import BeautifulSoup
+import urllib2
+
+
+Traceback (most recent call last):
+  File "<pyshell#0>", line 1, in <module>
+    from bs4 import BeautifulSoup
+ImportError: No module named bs4
+>>> 
